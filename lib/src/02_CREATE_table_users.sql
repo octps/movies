33ddc7b64c8dd647b8aea95b31db05c8e28d2921
octps <renmo9@gmail.com>
2016-05-25 19:30:01 +0900

    onetime_password カラム作成

--- lib/src/02_CREATE_table_users.sql
+++ lib/src/02_CREATE_table_users.sql
@@ -4,6 +4,7 @@ CREATE TABLE users (
   , email VARCHAR(64) NOT NULL
   , password VARCHAR(64) NOT NULL
   , auth int DEFAULT 0 /* 0は権限なし、1は全権限を持つ。以下、増えることに制限 */
+  , onetime_password VARCHAR(64) NOT NULL
   , created_at TIMESTAMP NOT NULL DEFAULT 0
   , updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW()
 ) ENGINE = InnoDB
--- lib/src/03_insert_users_dummy.sql
+++ lib/src/03_insert_users_dummy.sql
@@ -4,12 +4,14 @@ INSERT INTO users (
 	, email
 	, password
 	, auth
+	, onetime_password
 	, created_at
 ) values (
 	'北嶋'
 	, 'kitajima@toshin-k.com'
 	, 'f97db61df785e50ef95805f25fc4a6af78d96281' /* sha1 kitajima */
 	, 1
+	, 'test'
 	, null
 );
 
